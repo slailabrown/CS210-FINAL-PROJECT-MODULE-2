e6dff3b48c997dde972e5dfafdbc4e7874057d9c
slailabrown <sbrown8600@sdsu.edu>
2025-12-10 23:07:59 -0800

    Delete ReverseHangman directory

--- ReverseHangman/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ReverseHangman_FILE C:/Users/Laila/Documents/ReverseHangman/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/ReverseHangman.exe)
-set(__QT_DEPLOY_TARGET_ReverseHangman_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ReverseHangman_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
